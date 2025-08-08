3a55774cd26e179087eb5fdae31514360f53fc5c
mhmedgalal <mohmedgalal591@gmail.com>
2025-08-08 11:34:00 +0300

    final settings fix for deployment

--- project/settings.py
+++ project/settings.py
@@ -85,19 +85,19 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if os.getenv("DATABASE_URL"):  # لو فيه PostgreSQL URL (زي في Railway)
-    DATABASES = {
-        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-        
-    }
-else:  # تشغيل محلي باستخدام SQLite
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True  # Railway بيشتغل SSL
+    )
+}
 
+if not os.getenv("DATABASE_URL"):
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
